74098bac39c5b79a76cb962da75061256c59dd6c
Alexander A Cerlini <contato@alexcerlini.com.br>
2024-09-08 15:15:44 -0300

    feat: create home filter

---
+++ components/homeFilter/index.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { Input } from "@/components/shadcn/input";
+import { Eraser, SearchIcon } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../shadcn/tooltip";
+import { useForm } from "react-hook-form";
+import { useContext, useEffect } from "react";
+import { InputsProps } from "./types";
+import { HeroesContext } from "@/app/page";
+
+export const HomeFilter = () => {
+  const { updateResults } = useContext(HeroesContext);
+
+  const handleForm = (formData: InputsProps) => {
+    updateResults(formData.heroValue);
+  };
+
+  const handleClearForm = () => {
+    updateResults("");
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<InputsProps>();
+
+  useEffect(() => console.log({ errors }), [errors]);
+
+  return (
+    <form className="my-8 flex gap-4" onSubmit={handleSubmit(handleForm)}>
+      <Input placeholder="Enter your hero name" {...register("heroValue")} />
+
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <SearchIcon className="w-full h-full" />
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Search</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger type="reset" onClick={handleClearForm}>
+            <Eraser className="w-full h-full" />
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Clear</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </form>
+  );
+};
---
+++ components/homeFilter/types.ts
@@ -0,0 +1,3 @@
+export type InputsProps = {
+    heroValue: string;
+};
