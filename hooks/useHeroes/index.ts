29080fd2d3a74924152b92d3bf331e76b378c788
Alexander A Cerlini <contato@alexcerlini.com.br>
2024-09-08 22:57:35 -0300

    feat: create useHeroes hook with react query

---
+++ hooks/useHeroes/index.ts
@@ -0,0 +1,24 @@
+import { getMarvelAuthorization } from '@/lib/utils'
+import { useQuery } from '@tanstack/react-query'
+import { HeroesResponse } from './types'
+
+const fetchHeroes = async (page = 0): Promise<HeroesResponse> => {
+    const { ts, publicApiKey, hash, marvelBaseUrl } = getMarvelAuthorization()
+
+    const offset = page * 20
+
+    const response = await fetch(`${marvelBaseUrl}/v1/public/characters?ts=${ts}&apikey=${publicApiKey}&hash=${hash}&orderBy=name&limit=20&offset=${offset}`)
+    const data = await response.json()
+    return data
+}
+
+const useHeroes = (page: number) => {
+    return useQuery({
+        queryKey: ['heroes', page],
+        queryFn: () => fetchHeroes(page),
+        refetchOnWindowFocus: false,
+        staleTime: 1000 * 60 * 60 * 24,
+    })
+}
+
+export { useHeroes, fetchHeroes }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,18 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import crypto from "crypto"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const getMarvelAuthorization = () => {
+  const ts = 1
+  const publicApiKey = process.env.MARVEL_PUBLIC_API_KEY
+  const privateApiKey = process.env.MARVEL_PRIVATE_API_KEY
+  const marvelBaseUrl = process.env.MARVEL_BASE_URL
+
+  const hash = crypto.createHash('md5').update(`${ts}${privateApiKey}${publicApiKey}`).digest("hex")
+
+  return { ts, hash, publicApiKey, marvelBaseUrl }
 }
