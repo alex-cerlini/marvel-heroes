897b9c6b1e3da73422f092645d0e7cb9053e3e9f
Alexander A Cerlini <contato@alexcerlini.com.br>
2024-09-09 17:29:21 -0300

    feat: add not found heroes message

--- components/heroes/index.tsx
+++ components/heroes/index.tsx
@@ -8,12 +8,21 @@ export const Heroes = () => {
   const searchParams = useSearchParams();
   const page = Number(searchParams.get("page")) || 1;
 
-  const { data: response, isFetching } = useHeroes(page);
+  const { data: response, isFetching, isError } = useHeroes(page);
 
   if (isFetching) {
     return (
       <div className="min-h-[calc(100svh-320px)] flex items-center justify-center max-w-screen-xl w-full">
         <LoadingSpinner size={112} />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="min-h-[calc(100svh-320px)] flex items-center justify-center max-w-screen-xl w-full">
+        No results were found for your search. Please try again in a few
+        moments.
       </div>
     );
   }
