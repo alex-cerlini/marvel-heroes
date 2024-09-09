0babf0ff07dcfbab18ab766912b1c0d4e9b685b6
Alexander A Cerlini <contato@alexcerlini.com.br>
2024-09-08 22:55:29 -0300

    chore: add env with example

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    MARVEL_PUBLIC_API_KEY: process.env.MARVEL_PUBLIC_API_KEY,
+    MARVEL_PRIVATE_API_KEY: process.env.MARVEL_PRIVATE_API_KEY,
+    MARVEL_BASE_URL: process.env.MARVEL_BASE_URL,
+  },
+};
 
 export default nextConfig;
