63c9490cf687566652db059055a826166bcadda9
Alexander A Cerlini <contato@alexcerlini.com.br>
2024-09-09 17:15:15 -0300

    fix: add suspense on page to fix build

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { HomeFilter } from "@/components/homeFilter";
+import { LoadingSpinner } from "@/components/loadingSpinner";
 import { Providers } from "@/components/providers";
+import { Suspense } from "react";
 import { Heroes } from "@/components/heroes";
 import { Pagination } from "@/components/pagination";
 
@@ -8,8 +10,19 @@ export default async function Home() {
     <Providers>
       <div className="min-h-[calc(100svh-175px)] gap-4 flex flex-col justify-start max-w-screen-xl w-full">
         <HomeFilter />
-        <Heroes />
-        <Pagination />
+        <Suspense
+          fallback={
+            <div className="min-h-[calc(100svh-320px)] flex items-center justify-center max-w-screen-xl w-full">
+              <LoadingSpinner size={112} />
+            </div>
+          }
+        >
+          <Heroes />
+        </Suspense>
+
+        <Suspense>
+          <Pagination />
+        </Suspense>
       </div>
     </Providers>
   );
